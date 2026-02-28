a0745257cab48dff29b570f499a3db74ac6462ca
zjq <jqchu96@outlook.com>
2026-02-28 23:04:29 +0900

    Initial Commit

---
+++ QLPlainView/QLPlainViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QLPlainViewApp.swift
+//  QLPlainView
+//
+//  Created by neo on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct QLPlainViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
